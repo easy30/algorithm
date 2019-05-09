4da755336d22fd93f5e6133a194eed671fe18e53
liruidong <liruidong96@163.com>
2019-05-09 17:28:03 +0800

    Update Summation.java

--- 01-两数之和/Summation.java
+++ 01-两数之和/Summation.java
@@ -1,16 +1,17 @@
 package com.cehome;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Summation {
 
     public static void main(String[] args) {
-        System.out.println(findTowNum(new int[]{2, 7, 11, 15}, 9));
+        System.out.println(Arrays.toString(findTowNum(new int[]{2, 7, 11, 15}, 9)));
     }
 
-    public static String findTowNum(int[] num, int target) {
-        String result = "";
+    public static int[] findTowNum(int[] num, int target) {
+        int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         if (num.length > 1 && target > 0) {
             for (int i=0; i< num.length; i++) {
@@ -18,7 +19,8 @@ public class Summation {
                     Integer two = target - num[i];
                     Integer j = map.get(two);
                     if (j != null) {
-                        result = "[" + j + "," + i + "]";
+                        result[0] = j;
+                        result[1] = i;
                     }
                     map.put(num[i], i);
                 }
